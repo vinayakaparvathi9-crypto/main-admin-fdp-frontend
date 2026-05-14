0969abe7b85df98aa0df7f4a5461f20767048651
Hachathon98 <pavanivkj418@gmail.com>
2026-05-14 14:20:55 +0530

    Update API baseURL for production

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+    ? 'http://localhost:5000/api'
+    : 'https://main-admin-fdp-backend-3.onrender.com/api',
 });
 
 API.interceptors.request.use((req) => {
